894084c1abdf18967651f5f28d00b2f35463bc60
nurgal1ev <nurgaliev0644@gmail.com>
2025-11-04 13:36:11 +0300

    cfg

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"REST-Api/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := SetupLogger(cfg.Env)
+	log.Info("Starting server", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+}
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}))
+	}
+
+	return log
+}
